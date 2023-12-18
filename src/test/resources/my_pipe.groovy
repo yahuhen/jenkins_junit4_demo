340847e61e95fb8019358da836fa7cdecb8d8130
Yauhen_Shydlouski <yahuhen@gmail.com>
2023-12-18 18:15:39 +0300

    pipeline

---
+++ src/test/resources/my_pipe.groovy
@@ -0,0 +1,29 @@
+pipeline {
+    agent any
+
+    stages {
+        stage('Checkout') {
+            steps {
+                git branch: 'main', url: 'https://github.com/yahuhen/jenkins_junit4_demo.git'
+            }
+        }
+
+        stage('Build and Test') {
+            steps {
+                sh "mvn -Dmaven.test.failure.ignore=true clean test"
+            }
+        }
+
+        stage('Test Results') {
+
+            post {
+                // If Maven was able to run the tests, even if some of the test
+                // failed, record the test results and archive the jar file.
+                success {
+                    junit '**/target/surefire-reports/TEST-*.xml'
+                    archiveArtifacts 'target/*.jar'
+                }
+            }
+        }
+    }
+}
